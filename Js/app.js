6f781d6dbcfb4c1424dde614c70146436c1d8951
MKHARBECH <salah_ita@yahoo.fr>
2023-03-08 19:43:38 +0100

    fichier js et envoie message vers formspree

---
+++ Js/app.js
@@ -0,0 +1,25 @@
+
+var form = document.getElementById("my-form");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("my-form-status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        status.classList.add('success');
+        status.innerHTML = "Merci de m'avoir contacté";
+        form.reset()
+      }).catch(error => {
+        status.classList.add('error');
+        status.innerHTML = "Oops! probleme d'envoi";
+      });
+    }
+    form.addEventListener("submit", handleSubmit) 
+
+
